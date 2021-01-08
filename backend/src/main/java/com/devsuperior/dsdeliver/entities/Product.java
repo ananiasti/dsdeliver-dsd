020228a835163cdb229cca28f0795bd67ec4f90c
unknown <ananiasti@gmail.com>
2021-01-07 23:45:58 -0300

    GET /products

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Product.java
@@ -51,4 +51,45 @@ public class Product implements Serializable{
 		return true;
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImageUri() {
+		return imageUri;
+	}
+
+	public void setImageUri(String imageUri) {
+		this.imageUri = imageUri;
+	}
+
+
 }
