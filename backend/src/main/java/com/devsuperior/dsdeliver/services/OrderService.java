c3b68f7e160046d8c4fa976939561499f0397a92
unknown <ananiasti@gmail.com>
2021-01-08 14:51:25 -0300

    PUT /order/{id}/delivered

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -17,36 +17,44 @@ import com.devsuperior.dsdeliver.repositories.OrderRepository;
 import com.devsuperior.dsdeliver.repositories.ProductRepository;
 
 @Service
-public class OrderService{
-	
+public class OrderService {
+
 	@Autowired
 	private OrderRepository repository;
-	
+
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	@Transactional(readOnly = true)
-	public List<OrderDTO> findAll(){
-		
-	   List<Order> list = repository.findOrdersWithProducts();
-	   return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
-		
+	public List<OrderDTO> findAll() {
+
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+
 	}
 
 	@Transactional
-	public OrderDTO insert(OrderDTO dto){
-		
-	   Order order = new Order(null, dto.getAddress(),
-			                         dto.getLatitude(),
-			                         dto.getLongitude(),
-			                         Instant.now(), OrderStatus.PENDING);
-	   for (ProductDTO p : dto.getProducts()) {
-		   Product product = productRepository.getOne(p.getId());
-	       order.getProducts().add(product);
-	   }
-	   
-	   order = repository.save(order);
-	   return new OrderDTO(order);
+	public OrderDTO insert(OrderDTO dto) {
+
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(),
+				OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
-	
+
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		repository.save(order);
+		return new OrderDTO(order);
+
+	}
+
 }
